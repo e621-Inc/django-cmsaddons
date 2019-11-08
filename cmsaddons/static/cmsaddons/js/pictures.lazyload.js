ddb9e27abae4a4279b4da76bd75fffba2e82dc36
alaric <alaric@wullerot.ch>
2019-11-08 15:34:44 +0100

    new lazyload

--- cmsaddons/static/cmsaddons/js/pictures.lazyload.js
+++ cmsaddons/static/cmsaddons/js/pictures.lazyload.js
@@ -3,53 +3,102 @@ var PicturesLazyLoad = ( function() {
 
     return { init: lazy_loader };
 
-    function lazy_loader( selector, observer_options ) {
+    function lazy_loader( selector, options ) {
         // select elements with given selector or default (.lazyload )
-        var images = document.querySelectorAll( selector || '.lazyload' );
+        var elements = document.querySelectorAll( selector || '.lazyload' );
+        options = get_options( options || {} );
 
         if ( 'loading' in HTMLImageElement.prototype ) {
             // full modern browser lazyloading we do nothing
-            images.forEach( img => {
-                img.src = img.dataset.src;
-                img.classList.add( 'loading' );
-            } );
+            // set_browser_loader( elements );
+            set_observer( elements, options.observer );
         } else if( 'IntersectionObserver' in window ) {
             // modern js lazyloading
-            set_observer( images, observer_options );
+            set_observer( elements, options.observer );
         } else {
             // older browsers no lazyloading
-            set_fallback( images );
+            set_fallback( elements );
         }
     };
 
-    function set_observer( images, options ) {
-        options = options || {};
+    function get_options( options ) {
+        if( !options.observer ) {
+            options.observer = {
+                rootMargin: '0px',
+                threshold: 0.1
+            }
+        }
+        return options;
+    };
+
+
+    // Browser lazyloading ----------------------------------------------------
+
+    function set_browser_loader( elements ) {
+        elements.forEach( element => {
+            element.classList.add( 'loading' );
+            element.src = element.dataset.src;
+            load_element_init( element );
+        } );
+    };
+
+
+    // Js lazyloading ---------------------------------------------------------
+
+    function set_observer( elements, options ) {
         var observer = new IntersectionObserver( lazy_load, options );
-        for( var i = 0; i < images.length; ++i ) {
-            images[ i ].classList.add( 'loading' )
-            observer.observe( images[ i ] );
+        for( var i = 0; i < elements.length; ++i ) {
+            load_element_init( elements[ i ] );
+            observer.observe( elements[ i ] );
         }
 
-        function lazy_load( elements ) {
-            elements.forEach( load_element );
+        function lazy_load( entries ) {
+            for( var i = 0; i < entries.length; ++i ) {
+                load_element( entries[ i ] )
+            }
         };
 
-        function load_element( element ) {
-            if ( element.intersectionRatio > 0 ) {
-                element.target.src = element.target.dataset.src;
-                observer.unobserve( element.target );
+        function load_element( entry ) {
+            var element = entry.target;
+            if ( entry.intersectionRatio > 0 ) {
+                element.src = element.dataset.src;
+                element.addEventListener( 'load', load_element_end )
+                observer.unobserve( element );
             }
         };
     };
 
+
+    // Fallback no lazyload ---------------------------------------------------
+
     function set_fallback( images ) {
-        var i;
+        // TODO implement js loader for old version on scroll loading
         var img;
-        for( i = 0; i < images.length; ++i ) {
+        for( var i = 0; i < images.length; ++i ) {
             img = images[ i ];
             img.src = img.dataset.src;
             img.classList.remove( 'loading' )
         };
+    };
+
+
+    //  Utilities -------------------------------------------------------------
+
+    function load_element_init( element ) {
+        element.classList.add( 'loading' );
+        if( element.dataset.has_wrap ) {
+            element.parentNode.classList.add( 'wrap-loading' );
+        }
+    };
+
+    function load_element_end( event ) {
+        var element = event.target;
+        element.classList.remove( 'loading' );
+        element.classList.add( 'loaded' );
+        if( element.dataset.has_wrap ) {
+            element.parentNode.classList.remove( 'wrap-loading' );
+            element.parentNode.classList.add( 'wrap-loaded' );
+        }
     };
 
 } )();
